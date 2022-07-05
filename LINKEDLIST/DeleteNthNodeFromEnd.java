9f5d0db16af5808d5bc092826641263a042f0844
paras <parasverma2531@gmail.com>
2022-07-05 14:46:29 +0530

    delete Nth Node from End of the list

---
+++ LINKEDLIST/DeleteNthNodeFromEnd.java
@@ -0,0 +1,24 @@
+package LINKEDLIST;
+public class DeleteNthNodeFromEnd {
+    /*
+    Ques: delete nth node from the end of the list
+    Approach:: run the loop till n+1 and iterate fast;
+    then till fast!=null
+    iterate fast and slow
+    this will bring slow to one node before the node which is to deleted.d:
+    
+     */
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy= new ListNode(-1,head);
+        head=dummy;
+        ListNode slow=head,fast=head;
+        for(int i=1;i<=n+1;i++)
+            fast=fast.next;
+        while(fast!=null)
+        {
+            slow=slow.next;
+            fast=fast.next;
+        }slow.next=slow.next.next;
+        return dummy.next;
+    }
+}
