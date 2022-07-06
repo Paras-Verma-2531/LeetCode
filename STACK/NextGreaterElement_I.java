969490644ba7461a5c6154640f73d419f075a3a5
paras <parasverma2531@gmail.com>
2022-07-06 17:54:01 +0530

    Next Greater Element-1-in another array

---
+++ STACK/NextGreaterElement_I.java
@@ -0,0 +1,31 @@
+package STACK;
+import java.util.Stack;
+public class NextGreaterElement_I {
+    /*
+    Ques : array 1 is a subset of array 2: find the next greater element of array 1 and modify the array 1:
+    eg:
+    Input: nums1 = [4,1,2], nums2 = [1,3,4,2]
+    Output: [-1,3,-1]
+    Approach : Brute force[O(N^2)]
+    since the greater element will be to the right of element in array 2, therefore till i!=j push the elements in stack
+    now once the element are pushed, check if the stack is not empty and peek is <=nums[i],and pop the data
+    now check;  nums1[i]=stack.isEmpty()?-1:stack.peek();
+    and make the stack clear::
+    */
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer> stack= new Stack<>();
+        for(int i=0;i<nums1.length;i++)
+        {
+            int j=nums2.length-1;
+            while(nums1[i]!=nums2[j])
+            {
+                stack.push(nums2[j]);
+                j--;
+            }
+            while(!stack.isEmpty()&&stack.peek()<=nums1[i])
+                stack.pop();
+            nums1[i]=stack.isEmpty()?-1:stack.peek();
+            stack.clear();
+        }return nums1;
+    }
+}
