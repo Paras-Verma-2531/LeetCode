4b45f93008c4c707414d9df6ff1b9a3a0e8a5a29
paras <parasverma2531@gmail.com>
2022-07-20 11:00:13 +0530

    AddTwoNumbers

--- LINKEDLIST/Add_Two_Numbers.java
+++ LINKEDLIST/Add_Two_Numbers.java
@@ -5,7 +5,7 @@ public class Add_Two_Numbers {
     Ques: You are given two non-empty linked lists representing two non-negative integers.
     The digits are stored in reverse order, and each of their nodes contains a single digit.
      Add the two numbers and return the sum as a linked list.
-     
+
      */
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode dummy=new ListNode(-1);
