bd4e93bed9cd484000735e89b8df8b089fe61395
paras <parasverma2531@gmail.com>
2022-07-22 18:02:45 +0530

    RotateToRight

---
+++ LINKEDLIST/RotateToRight.java
@@ -0,0 +1,31 @@
+package LINKEDLIST;
+public class RotateToRight {
+    /*
+    Ques: 
+     */
+    public ListNode insertAtHead(ListNode head)
+    {
+        ListNode newhead=null;
+        ListNode curr=head;
+        while(curr.next.next!=null)
+            curr=curr.next;
+        newhead=curr.next;
+        curr.next=null;
+        newhead.next=head;
+        return newhead;
+    }
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head==null||head.next==null)
+            return head;
+        ListNode temp=head;int len=0;
+        while(temp!=null)// calculate length
+        {
+            len++;temp=temp.next;
+        }
+        ListNode newhead=head;int i=0;
+        if(len<k)k=k%len;// prevent from TLE
+        while(i++<k)
+            newhead=insertAtHead(newhead);
+        return newhead;
+    }
+}
