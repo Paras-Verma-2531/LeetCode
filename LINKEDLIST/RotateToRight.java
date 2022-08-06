ae701359897f3d07546e7511dcbf1b35338ea279
paras <parasverma2531@gmail.com>
2022-08-06 18:55:52 +0530

    Rotate the LinkedList

--- LINKEDLIST/RotateToRight.java
+++ LINKEDLIST/RotateToRight.java
@@ -2,6 +2,8 @@ package LINKEDLIST;
 public class RotateToRight {
     /*
     Ques: Given the head of a linked list, rotate the list to the right by k places.
+    Input: head = [1,2,3,4,5], k = 2
+    Output: [4,5,1,2,3]
      */
     public ListNode insertAtHead(ListNode head)
     {
