3fb58932aa9e26f59155a71c6adce09906f05760
paras <parasverma2531@gmail.com>
2022-08-07 13:38:11 +0530

    RemoveZeroSumSubList

---
+++ LINKEDLIST/RemoveZeroSumSubList.java
@@ -0,0 +1,33 @@
+package LINKEDLIST;
+import java.util.HashMap;
+import java.util.Map;
+public class RemoveZeroSumSubList {
+    /*
+    Ques::Given the head of a linked list, we repeatedly delete consecutive sequences of nodes that sum to 0
+    until there are no such sequences.
+After doing so, return the head of the final linked list.  You may return any such answer
+     */
+    public ListNode removeZeroSumSublists(ListNode head) {
+        Map<Integer,ListNode> map=new HashMap<>();
+        ListNode dummy=new ListNode(0);
+        dummy.next=head;
+        ListNode temp=dummy;int sum=0;
+        while(temp!=null)
+        {
+            sum+=temp.val;
+            map.put(sum,temp);
+            temp=temp.next;
+        }sum=0;
+        temp=dummy;
+        while(temp!=null)
+        {
+            sum+=temp.val;
+            if(map.containsKey(sum))
+            {
+                ListNode node=map.get(sum);
+                if(temp!=node)
+                    temp.next=node.next;
+            }temp=temp.next;
+        }return dummy.next;
+    }
+}
