f796ec15f5cbb64196c775f3d1c4f45b65808b44
paras <parasverma2531@gmail.com>
2022-08-07 15:06:40 +0530

    DeleteMiddleElement

--- LINKEDLIST/deleteMiddleElement.java
+++ LINKEDLIST/deleteMiddleElement.java
@@ -3,9 +3,9 @@ package LINKEDLIST;
 public class deleteMiddleElement {
     /*
     Ques: delete the middle element of the list:
-    Approach :: if list has only one node, return null and if two make the head next as null and return;
+    Approach :: if list has only one node, return null and if two,make the head next as null and return;
     else::
-    follow the floyd's cycle detection aglo for middle element and make the changes;
+    follow the floyd's cycle detection algo for middle element and make the changes;
      */
     public ListNode deleteMiddle(ListNode head) {
         if(head.next==null)
@@ -22,7 +22,7 @@ public class deleteMiddleElement {
             fast=fast.next.next;
         }
         slow.val=slow.next.val;
-        slow.next=slow.next.next;
+        slow.next=slow.next.next;//can also be achieved through third variable
         return head;
     }
 }
