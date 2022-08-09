cae54143e37d7aa679c9a7a3c19fa48b31a0012c
paras <parasverma2531@gmail.com>
2022-08-10 00:53:14 +0530

    ConvertBinaryListToInteger

---
+++ LINKEDLIST/ConvertBinaryListToInteger.java
@@ -0,0 +1,26 @@
+package LINKEDLIST;
+public class ConvertBinaryListToInteger {
+    /*
+    Ques: Given head which is a reference node to a singly-linked list.
+    The value of each node in the linked list is either 0 or 1. The linked list holds the binary representation of a number.
+Return the decimal value of the number in the linked list
+     */
+    public ListNode reverse(ListNode head)//reverse the list
+    {
+        if(head.next==null)return head;
+        ListNode newhead=reverse(head.next);
+        ListNode node=head.next;
+        node.next=head;
+        head.next=null;
+        return newhead;
+    }
+    public int getDecimalValue(ListNode head) {
+        head=reverse(head);
+        int sum=0,i=0;
+        while(head!=null)
+        {
+            sum+=head.val*Math.pow(2,i++);// converts the no into integer
+            head=head.next;
+        }return sum;
+    }
+}
