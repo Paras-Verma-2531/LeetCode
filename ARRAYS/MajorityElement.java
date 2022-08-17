7e2a6e3c4a151e1ed0c4506d5e9f47beb8d3ef58
paras <parasverma2531@gmail.com>
2022-08-17 23:45:31 +0530

    MOORE'S VOTING ALGO- majorityElement

--- ARRAYS/MajorityElement.java
+++ ARRAYS/MajorityElement.java
@@ -7,7 +7,8 @@ public class MajorityElement {
     Ques::Given an array nums of size n, return the majority element.
 The majority element is the element that appears more than ⌊n / 2⌋ times.
 You may assume that the majority element always exists in the array.
-     */
+    ---------------------------------------------------
+    ---------------------------------------------------
     public int majorityElement(int[] nums) {
         // [[[[[[[  ***** MY APPROACH******]]]]]]
         Map<Integer,Integer> map=new HashMap<>();
@@ -22,5 +23,40 @@ You may assume that the majority element always exists in the array.
             if(map.get(val)>(nums.length/2))return val;
         }return 0;
     }
+    */
+    // Optimized solution: Time Comp: O(n),,Space comp: O(1)
+    //[["MOORE'S VOTING ALGO"]]
+    /*
+    The index i,e ind will give the majority element
+    we assume the first element is the majority element
+    make its count as 1 and ind as 0:
+    now traverse through the array and if arr[i] == arr[ind] make count++
+    else make count --;
+    if(count==0) change count to 1 and ind to i
+    --------------
+    // traverse the loop again and check if count of arr[ind] is >n/2::
+    if yes, return the element
+     */
+    public int majorityElement(int[] nums) {
+        int ansInd=0;
+        int count=1;
+        for(int i=1;i<nums.length;i++)
+        {
+            if(nums[i]==nums[ansInd])
+                count++;
+            else
+                count--;
+            if(count==0)
+            {
+                ansInd=i;count=1;
+            }
+        }count=0;
+        for(int i=0;i<nums.length;i++)
+            if(nums[ansInd]==nums[i])
+                count++;
+        if(count>(nums.length/2))return nums[ansInd];
+        return 0;
+    }
+
 }
 
