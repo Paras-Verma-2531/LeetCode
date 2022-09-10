89196a1fdba4aa283c0e102780cafbcfd4c2db64
paras <parasverma2531@gmail.com>
2022-09-10 19:55:00 +0530

    Find All Disappeared Number in Array

--- ARRAYS/Find_All_Disappeared_number.java
+++ ARRAYS/Find_All_Disappeared_number.java
@@ -2,6 +2,13 @@ package ARRAYS;
 import java.util.ArrayList;
 import java.util.List;
 public class Find_All_Disappeared_number {
+    /*
+    Ques: Given an array nums of n integers where nums[i] is in the range [1, n],
+    return an array of all the integers in the range [1, n] that do not appear in nums.
+    Example 1:
+Input: nums = [4,3,2,7,8,2,3,1]
+Output: [5,6]
+     */
     public static int swap(int[]arr,int i,int j)
     {//A basic swap method to swap values at two indices
         int temp=arr[i];
