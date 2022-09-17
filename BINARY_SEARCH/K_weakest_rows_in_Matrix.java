f9e6dfac8aa2c540d744c4bf697cac7fb104bdd2
paras <parasverma2531@gmail.com>
2022-09-17 19:00:37 +0530

    K Weakest Rows in Matrix

---
+++ BINARY_SEARCH/K_weakest_rows_in_Matrix.java
@@ -0,0 +1,55 @@
+package BINARY_SEARCH;
+public class K_weakest_rows_in_Matrix {
+    /*
+    Ques: You are given an m x n binary matrix mat of 1's (representing soldiers) and 0's (representing civilians).
+    The soldiers are positioned in front of the civilians. That is, all the 1's will appear to the left of all the 0's in each row.
+A row i is weaker than a row j if one of the following is true:
+The number of soldiers in row i is less than the number of soldiers in row j.
+Both rows have the same number of soldiers and i < j.
+Return the indices of the k weakest rows in the matrix ordered from weakest to strongest.
+     */
+    public int binarySearch(int[]arr,int start,int end)
+    {
+        while(start<end)
+        {
+            int mid=start+(end-start)/2;
+            if(arr[mid]==0)end=mid;
+            else
+                start=mid+1;
+        }return start;
+    }
+    public void sorting(int[]arr,int[]idxarr,int size)
+    {
+        for(int i=0;i<size;i++)
+        {
+            for(int j=0;j<size-i;j++)
+            {
+                if(arr[j]>arr[j+1])
+                {
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                    //swap index
+                    int pos=idxarr[j];
+                    idxarr[j]=idxarr[j+1];
+                    idxarr[j+1]=pos;
+                }
+            }
+        }
+    }
+    public int[] kWeakestRows(int[][] mat, int k) {
+        int[]arr=new int[mat.length];
+        int []idxarr=new int[mat.length];
+        for(int i=0;i<mat.length;i++)
+            arr[i]=binarySearch(mat[i],0,mat[i].length);
+        int j=0;
+        while(j<idxarr.length)
+            idxarr[j]=j++;
+        sorting(arr,idxarr,arr.length-1);
+        int[] newarr=new int[k];
+        for(int i=0;i<k;i++)
+            newarr[i]=idxarr[i];
+        return newarr;
+
+    }
+}
