187bad5d7f8b1ab7a4a863f013b181f273fd23e0
paras <parasverma2531@gmail.com>
2022-09-23 02:12:42 +0530

    Count Rotations

--- BINARY_SEARCH/Count_RotatedArray.java
+++ BINARY_SEARCH/Count_RotatedArray.java
@@ -4,7 +4,7 @@ public class Count_RotatedArray {
     Ques: Given an ascending sorted rotated array Arr of distinct integers of size N.
      The array is right rotated K times. Find the value of K.
      */
-    //Approach :: return the pivot index +1 :: that will be the answer becoz in sorted Array pivot would be at last, and if it is rotated it woul be at some index
+    //Approach :: return the pivot index +1 :: that will be the answer becoz in sorted Array pivot would be at last, and if it is rotated it would be at some index
     // which tells how many times a array is rotated
     int findKRotation(int arr[], int n) {
         int start=0,end=n-1;
