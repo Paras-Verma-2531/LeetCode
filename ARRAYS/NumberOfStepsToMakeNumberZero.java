3b4010967724b8ef182f2ff836d0aa3e0bbc92c7
paras <parasverma2531@gmail.com>
2022-10-23 23:33:20 +0530

    NumberOfStepsToMakeNumberZero

--- ARRAYS/NumberOfStepsToMakeNumberZero.java
+++ ARRAYS/NumberOfStepsToMakeNumberZero.java
@@ -1,7 +1,8 @@
 package ARRAYS;
 public class NumberOfStepsToMakeNumberZero {
     /*
-    Ques: Given an integer num, return the number of steps to reduce it to zero.
+    Ques:https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+    Given an integer num, return the number of steps to reduce it to zero.
 In one step, if the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it
      */
     public int numberOfSteps(int num) {
@@ -12,4 +13,15 @@ In one step, if the current number is even, you have to divide it by 2, otherwis
             num=num%2==0?num/2:num-1;
         }return count;
     }
+    /*
+    Using recursion
+      static int helper(int n,int count)
+    {
+        if(n==0)return count;
+        return (n&1)==1?helper(n-1,count+1):helper(n/2,count+1);
+    }
+    public int numberOfSteps(int num) {
+        return helper(num,0);
+    }
+     */
 }
