14fdadb843f15a66494e849bec37e21405ad67fa
paras <parasverma2531@gmail.com>
2022-10-27 19:31:37 +0530

    Detect Cycle

--- LINKEDLIST/DetectCycle.java
+++ LINKEDLIST/DetectCycle.java
@@ -6,19 +6,17 @@ if at any point fast==slow [cycle is present]
 */
 public class DetectCycle {
     public boolean hasCycle(ListNode head) {
-        boolean cycle = false;
         ListNode slow = head, fast = head;
         if (head == null)
-            return cycle;
+            return false;
         while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
             if (slow == fast) {
-                cycle = true;
-                break;
+                return true;
             }
         }
-        return cycle;
+        return false;
     }
 }
 
