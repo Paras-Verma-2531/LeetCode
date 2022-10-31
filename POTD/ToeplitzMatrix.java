aa3a02890557e14672beb4e502c2687b39d49726
paras <parasverma2531@gmail.com>
2022-10-31 13:37:39 +0530

    October-31

---
+++ POTD/ToeplitzMatrix.java
@@ -0,0 +1,15 @@
+package POTD;
+public class ToeplitzMatrix {
+    /*
+    Ques : https://leetcode.com/problems/toeplitz-matrix/
+     */
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        for(int i=1;i<matrix.length;i++)
+        {
+            for(int j=1;j<matrix[i].length;j++)
+            {
+                if(matrix[i][j]!=matrix[i-1][j-1])return false;
+            }
+        }return true;
+    }
+}
