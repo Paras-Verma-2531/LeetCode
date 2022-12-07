4ec87672440e3ca29cd1895037cea5ddb81d5d79
paras verma <parasverma2531@gmail.com>
2022-12-07 20:38:14 +0530

    Update ConcatenationOfArray.java

--- ARRAYS/ConcatenationOfArray.java
+++ ARRAYS/ConcatenationOfArray.java
@@ -4,7 +4,7 @@ public class ConcatenationOfArray {
     Ques:: Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i]
     and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
 Specifically, ans is the concatenation of two nums arrays.
-Return the array ans
+Return the array ans:
      */
     public int[] getConcatenation(int[] nums) {
         int[] newarr=new int[2*nums.length];
