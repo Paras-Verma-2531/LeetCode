26661887284a048295844c89c03c757d9994f635
paras <parasverma2531@gmail.com>
2023-01-21 18:56:23 +0530

    Subset II

--- RECURRSION/SubSet_II.java
+++ RECURRSION/SubSet_II.java
@@ -6,17 +6,37 @@ public class SubSet_II {
     /*
     Ques: https://leetcode.com/problems/subsets-ii/
      */
+    //bruteforce Approach:: if the list contains the same list do not add it otherwise add:
+//    private void helper(int[]arr,int index,List<List<Integer>>finalList,List<Integer>ans)
+//    {
+//        if(finalList.contains(ans))return;
+//        if(index==arr.length){finalList.add(new ArrayList<>(ans));return;}
+//        ans.add(arr[index]);
+//        helper(arr,index+1,finalList,ans);
+//        ans.remove(ans.size()-1);
+//        helper(arr,index+1,finalList,ans);
+//    }
+//    public List<List <Integer>> subsetsWithDup(int[] nums) {
+//        List<List<Integer>>finalList=new ArrayList <>();
+//        Arrays.sort(nums);
+//        helper(nums,0,finalList,new ArrayList<Integer>());
+//        return finalList;
+//    }
+    // Optimised Approach:
     private void helper(int[]arr,int index,List<List<Integer>>finalList,List<Integer>ans)
     {
-        if(finalList.contains(ans))return;
-        if(index==arr.length){finalList.add(new ArrayList<>(ans));return;}
-        ans.add(arr[index]);
-        helper(arr,index+1,finalList,ans);
-        ans.remove(ans.size()-1);
-        helper(arr,index+1,finalList,ans);
+        finalList.add(new ArrayList<>(ans));
+        for(int i=index;i<arr.length;i++)
+        {
+            if(i!=index&&arr[i]==arr[i-1])continue;// if at each level we have same elements i,e == i-1 do not pick them
+            ans.add(arr[i]);
+            helper(arr,i+1,finalList,ans);
+            ans.remove(ans.size()-1);
+
+        }
     }
-    public List<List <Integer>> subsetsWithDup(int[] nums) {
-        List<List<Integer>>finalList=new ArrayList <>();
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>>finalList=new ArrayList<>();
         Arrays.sort(nums);
         helper(nums,0,finalList,new ArrayList<Integer>());
         return finalList;
