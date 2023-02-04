b3b8018288486fdd990001e8a39b018b31bb2743
paras <parasverma2531@gmail.com>
2023-02-04 22:43:18 +0530

    BinaryList To Integer;

--- LINKEDLIST/ConvertBinaryListToInteger.java
+++ LINKEDLIST/ConvertBinaryListToInteger.java
@@ -1,7 +1,8 @@
 package LINKEDLIST;
 public class ConvertBinaryListToInteger {
     /*
-    Ques: Given head which is a reference node to a singly-linked list.
+    Ques: https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/
+    Given head which is a reference node to a singly-linked list.
     The value of each node in the linked list is either 0 or 1. The linked list holds the binary representation of a number.
 Return the decimal value of the number in the linked list
      */
