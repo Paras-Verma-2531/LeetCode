19576d9aade2ee7801ed204d29699a20c3686179
paras verma <parasverma2531@gmail.com>
2023-07-14 01:24:22 +0530

    Top View of BT

---
+++ TREE/Pair.java
@@ -0,0 +1,10 @@
+package TREE;
+public class Pair {
+    int hd;
+    TreeNode node;
+    public Pair(int hd, TreeNode node) {
+        this.hd = hd;
+        this.node = node;
+    }
+}
+
---
+++ TREE/TopView.java
@@ -0,0 +1,23 @@
+package TREE;
+import java.util.*;
+public class TopView {
+    public ArrayList <Integer> topView(TreeNode root) {
+        // Use level Order traversal :: if horizontal dis already present don't add node of that dis
+        Queue <Pair> queue = new ArrayDeque <>();//since we need element with horizontal dis,root therefore create a new class
+        Map <Integer, Integer> map = new TreeMap <>();
+        queue.add(new Pair(0, root));
+        while (!queue.isEmpty()) {
+            Pair pair = queue.poll();
+            if (!map.containsKey(pair.hd)) {
+                map.put(pair.hd, pair.node.val);
+            }
+            if (pair.node.left != null) queue.offer(new Pair(pair.hd - 1, pair.node.left));
+            if (pair.node.right != null) queue.offer(new Pair(pair.hd + 1, pair.node.right));
+        }
+        ArrayList <Integer> ans = new ArrayList <>();
+        for (int key : map.keySet()) {
+            ans.add(map.get(key));
+        }return ans;
+    }
+}
+
